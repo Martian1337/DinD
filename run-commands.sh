92cbe5a0c871ebce26247909b525b9af51e184c2
Martian <73861544+Martian1337@users.noreply.github.com>
2024-09-29 19:52:24 -0500

    Delete run-commands.sh

--- run-commands.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This script will run commands inside the sub-docker
-
-# Run your commands here
-echo "Running initial commands inside the sub-docker..."
-curl https://example.com
-# Add more commands as needed
